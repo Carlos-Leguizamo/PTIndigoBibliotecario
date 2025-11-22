b15b8192af5d5be22bdbd97c14504c010a1d1cda
Carlos Leguizamo <126182094+Carlos-Leguizamo@users.noreply.github.com>
2025-11-22 10:35:39 -0500

    feat: añade ruta de registro y flujo para el mismo

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,7 +5,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrestamoController;
 use App\Http\Controllers\Api\AuthController;
 
+use App\Http\Controllers\Api\RegisterController;
+
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/register', [RegisterController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
